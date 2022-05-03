5eb6531a95cb55f4cc244b7339ee67a6619285d3
Bobby Gerace <bobby.gerace@world50.com>
2022-05-03 16:00:57 -0400

    more tmux stuff, and fix copy/paste in nvim

--- .config/nvim/lua/config/tmux.lua
+++ .config/nvim/lua/config/tmux.lua
@@ -4,7 +4,7 @@ require("tmux").setup {
   copy_sync = {
     -- enables copy sync and overwrites all register actions to
     -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
-    enable = true,
+    enable = false,
   },
   navigation = {
     -- enables default keybindings (C-hjkl) for normal mode
