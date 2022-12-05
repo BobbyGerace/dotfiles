5b38bccbe67a885879676f99c714d4492ad1b383
Bobby Gerace <bobby.gerace@world50.com>
2022-12-05 13:43:46 -0500

    Add catppuccin theme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -7,12 +7,10 @@ require('packer').startup(function(use)
   use { 'wbthomason/packer.nvim' }
 
   -- Themes
-  -- To switch themes
-  --  1. uncomment the get_config line (if there is one)
-  --  2. change g._theme variable in options.lua
+  -- To switch themes, change g._theme variable in options.lua
   use {
     'EdenEast/nightfox.nvim',
-    -- config = get_config('nightfox')
+    config = get_config('nightfox')
   }
   use { 'michaeldyrynda/carbon' }
   use { 'folke/tokyonight.nvim' }
@@ -22,7 +20,11 @@ require('packer').startup(function(use)
   }
   use {
     'Mofiqul/dracula.nvim',
-    -- config = get_config('dracula')
+    config = get_config('dracula')
+  }
+  use { "catppuccin/nvim", 
+    as = "catppuccin",
+    config = get_config('catppucin')
   }
 
   -- pickers for search / grep / etc
