273cf9bea9106da8a05f1ab390c13511ca30be1f
Bobby Gerace <bobby.gerace@world50.com>
2023-01-04 09:41:41 -0800

    Some aerial changes

--- .config/nvim/lua/config/aerial.lua
+++ .config/nvim/lua/config/aerial.lua
@@ -3,11 +3,12 @@ local map = require('util').map
 require('aerial').setup({
   backends = {'lsp'},
   filter_kind = false,
-  close_on_select = true,
-  max_width = nil,
-  min_width = nil,
-  width = 40,
-  show_guides = true
+  show_guides = true,
+  layout = {
+    max_width = nil,
+    min_width = nil,
+    width = 40,
+  }
 })
 
 map('n', '<leader>o', '<cmd>AerialToggle<cr>', 'open symbol outline')
