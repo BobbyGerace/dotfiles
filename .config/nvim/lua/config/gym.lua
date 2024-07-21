af7d6d7516185720cc80279723cf05780099dbc8
Bobby Gerace <rfgerace@gmail.com>
2024-07-20 21:49:17 -0400

    Finish gym mappings

--- .config/nvim/lua/config/gym.lua
+++ .config/nvim/lua/config/gym.lua
@@ -1,5 +1,5 @@
 local gym = require('gym')
-local gym = require('gym.workout')
+local calc = require('gym.calc')
 local telescope = require('telescope')
 local map = require('util').map
 
@@ -11,4 +11,9 @@ map('n', '<leader>gwh', function() telescope.extensions.gym.workout_history() en
 map('n', '<leader>gwn', function() telescope.extensions.gym.current_workout_name_history() end, 'Named Workout History')
 map('n', '<leader>gep', function() telescope.extensions.gym.prs() end, 'Exercise PRs')
 
--- TODO: Add mappings for calc and workout commands
+local fill_convert_command = function()
+  vim.api.nvim_feedkeys(':GymConvertCurrent ', 'n', false)
+end
+
+map('n', '<leader>gce', calc.current_e1rm, 'Estimate 1RM')
+map('n', '<leader>gcc', fill_convert_command, 'Convert Set')
