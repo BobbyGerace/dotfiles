aff5be37c02d06d591d27b6e83bdfbcb30ef1b1f
Bobby Gerace <rfgerace@gmail.com>
2024-07-23 10:49:43 -0700

    More picker changes

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -7,7 +7,7 @@ local actions = require('telescope.actions')
 telescope.setup {
   defaults = themes.get_dropdown({
     file_ignore_patterns = { "node_modules", '.git/' },
-    path_display = { 'truncate' },
+    path_display = { 'filename_first' },
     mappings = {
       i = {
         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
@@ -34,8 +34,8 @@ telescope.setup {
     find_files = {
       hidden = true
     },
-    lsp_references = {
-      layout_strategy = 'vertical'
+    oldfiles = {
+      initial_mode = 'normal',
     },
   },
   extensions = {
@@ -78,7 +78,7 @@ map('n', '<leader>p', function() builtin.find_files() end, 'find files')
 map('n', 'gr', function() builtin.lsp_references() end, 'go to references')
 map('n', 'gd', function() builtin.lsp_definitions() end, 'go to definitions')
 map('n', '<leader>l', function() builtin.buffers() end, 'view open buffers')
-map('n', '<leader>h', function() builtin.oldfiles({ only_cwd = true }) end, 'view file history')
+map('n', '<leader>j', function() builtin.oldfiles({ only_cwd = true }) end, 'view file history')
 map('n', '<leader>gs', function() builtin.git_status() end, 'view git status')
 map('n', '<leader>e', function() builtin.diagnostics() end, 'view diagnostics')
 -- map('n', '<leader>f', function() builtin.live_grep({ hidden = true }) end, 'find in files')
